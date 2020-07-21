4796f565822d7534d546421ca8da0ff7888fbf5e
Stephen Feagin <stephenfeagin@gmail.com>
2020-07-20 21:13:37 -0400

    Add 'Invalid Input' test case for DatePublished

--- datePublished_test.go
+++ datePublished_test.go
@@ -7,18 +7,21 @@ import (
 
 func TestParseDatePublishedJSON(t *testing.T) {
 	want := time.Date(2019, 4, 2, 0, 0, 0, 0, time.UTC)
-	tests := map[string]string{
-		"Full Datetime": "2019-04-02T00:00:00.000Z",
-		"Date Only":     "2019-04-02",
+
+	tests := []struct {
+		name, input string
+		want        time.Time
+	}{
+
+		{"Full Datetime", "2019-04-02T00:00:00.000Z", want},
+		{"Date Only", "2019-04-02", want},
+		{"Invalid Input", "xxx", time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC)},
 	}
 
-	for name, input := range tests {
-		t.Run(name, func(t *testing.T) {
-			datePublished, err := parseDatePublishedFromJSON(input)
-			if err != nil {
-				t.Fatal(err)
-			}
-			if datePublished != want {
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			datePublished, _ := parseDatePublishedFromJSON(test.input)
+			if datePublished != test.want {
 				t.Fatal("incorrectly parsed DatePublished")
 			}
 		})
